559730def4a4adcf93d834bae6eb44833719fa3f
Julia Pulova <juliapulova@yandex.ru>
2015-02-03 16:44:30 +0200

    Spring Tutorial 34
    AOP XML configuration

--- src/org/julia/bdgolka/aspect/LogginAspect.java
+++ src/org/julia/bdgolka/aspect/LogginAspect.java
@@ -1,55 +1,23 @@
 package org.julia.bdgolka.aspect;
 
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
-import org.julia.bdgolka.model.Circle;
 
-@Aspect
 public class LogginAspect {
 
-	@Before("allCircleMethods()")
-	public void LoggingAdvice(JoinPoint jointPoint) {		
-		Circle circle = (Circle) jointPoint.getTarget();
-	}
-	
-	@AfterReturning(pointcut = "args(name)", returning="returnString")
-	public void stringArgumentMethods(String name, Object returnString){
-		System.out.println("A method that takes String arguments has called. The volue is " + name + " The output value is " + returnString);		
-	}
-	
-	@AfterThrowing(pointcut="args(name)", throwing = "ex")
-	public void exceptionAdvice(String name, Exception ex){
-		System.out.println("An exception has been thrown "+ ex);
-	}
-	
-	@Around("@annotation(org.julia.bdgolka.aspect.Loggable)")
-	public Object myAroundAdvice(ProceedingJoinPoint proceedingJoinPoint){
+	public Object myAroundAdvice(ProceedingJoinPoint proceedingJoinPoint) {
 		Object returnValue = null;
-			try {
-				System.out.println("Before advice");
-				returnValue = proceedingJoinPoint.proceed();
-				System.out.println("After Returning");
-				} catch (Throwable e) {
-				System.out.println("After throwing");
-			}	
-			
-			System.out.println("After Finally");
-			return returnValue;
-	}
+		try {
+			System.out.println("Before advice");
+			returnValue = proceedingJoinPoint.proceed();
+			System.out.println("After Returning");
+		} catch (Throwable e) {
+			System.out.println("After throwing");
+		}
 
-	@Pointcut("execution(* get*())")
-	public void allGetters() {
-	}
-	
-	@Pointcut("within(org.julia.bdgolka.model.Circle)")
-	public void allCircleMethods() {
+		System.out.println("After Finally");
+		return returnValue;
 	}
 
 }
